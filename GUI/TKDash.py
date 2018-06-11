a0e638b2eda66ee9e000460999affaad448f90b5
WillEhman <will@ehman.com>
2018-06-10 21:15:17 -0600

    Continued dash work

--- GUI/TKDash.py
+++ GUI/TKDash.py
@@ -7,9 +7,9 @@ upperLeftLabel = 'WATER'
 upperLeftValue = 0.0
 lowerLeftLabel = 'AFR'
 lowerLeftValue = 0.0
-upperCentreLabel = 'SPEED'
+upperCentreLabel = 'RPM'
 upperCentreValue = 0.0
-lowerCentreLabel = 'GEAR'
+lowerCentreLabel = 'SPEED'
 lowerCentreValue = 0.0
 upperRightLabel = 'OIL T'
 upperRightValue = 0.0
@@ -29,7 +29,11 @@ class MyFirstGUI:
         self.masterFrame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=N + E + S + W)
 
         self.rpmFrame = Frame(self.masterFrame, bd=0, relief=FLAT, background='black')
-        self.rpmFrame.grid(column=0, row=0, columnspan=3, sticky=N)
+        self.rpmFrame.grid(column=0, row=0, columnspan=3, sticky=W)
+
+        self.canvas = Canvas(self.rpmFrame, width=800, height=82, highlightthickness=0)
+        self.canvas.grid(column=0, row=0, columnspan=3, sticky=W)
+        self.canvas.create_line(0, 0, 200, 100, fill="red", dash=(4, 4))
 
         self.leftFrame = Frame(self.masterFrame, bd=0, relief=FLAT, background='black')
         self.leftFrame.grid(column=0, row=1, sticky=W)
@@ -40,7 +44,7 @@ class MyFirstGUI:
         self.rightFrame = Frame(self.masterFrame, bd=0, relief=FLAT, background='black')
         self.rightFrame.grid(column=2, row=1, sticky=W)
 
-        self.bottomFrame = Frame(self.masterFrame, bd=0, relief=FLAT, background='black')
+        self.bottomFrame = Frame(self.masterFrame, bd=0, relief=FLAT, background='black', highlightthickness=0)
         self.bottomFrame.grid(column=0, row=2, columnspan=3, sticky=W)
 
     def init_all_dials(self, master):
@@ -48,83 +52,99 @@ class MyFirstGUI:
         height = 1
         font = 'Arial'
         padx = 60
+        pady = 5
         smallFontSize = 24
         bigFontSize = 72
-        self.rpmBarBackground = Button(self.rpmFrame, width=113, height=9, background='black')
-        self.rpmBarBackground.grid(column=0, row=0, columnspan=3, sticky=W)
+        # self.canvas.create_rectangle(0, 0, 800, 40, fill='black')
 
-        self.rpmBar = Button(self.rpmFrame, width=0, height=7, background='green')
-        self.rpmBar.grid(column=0, row=0, columnspan=3, sticky=W)
-
-        self.bottomBar = Button(self.bottomFrame, width=0, height=7, background='black')
+        self.bottomBar = Button(self.bottomFrame, width=113, height=7, background='black')
         self.bottomBar.grid(column=0, row=0, columnspan=3, sticky=W)
 
-        self.upperLeftLabel = Label(self.leftFrame, text=upperLeftLabel, width=width, padx=padx, background='black',
+        self.upperLeftLabel = Label(self.leftFrame, text=upperLeftLabel, width=width, padx=padx, pady=pady,
+                                    background='black',
                                     foreground='white')
         self.upperLeftLabel.grid(column=0, row=0, sticky=W)
         self.upperLeftLabel.config(font=(font, smallFontSize))
-        self.upperLeftValue = Label(self.leftFrame, text=upperLeftValue, height=height, padx=padx, background='black',
+        self.upperLeftValue = Label(self.leftFrame, text=upperLeftValue, height=height, padx=padx, pady=pady,
+                                    background='black',
                                     foreground='white')
         self.upperLeftValue.grid(column=0, row=1, sticky=W)
         self.upperLeftValue.config(font=(font, bigFontSize))
 
-        self.upperCentreLabel = Label(self.centreFrame, text=upperCentreLabel, width=width, padx=padx,
+        self.upperCentreLabel = Label(self.centreFrame, text=upperCentreLabel, width=width, pady=pady, padx=padx,
                                       background='black',
                                       foreground='white')
         self.upperCentreLabel.grid(column=0, row=0, sticky=N)
         self.upperCentreLabel.config(font=(font, smallFontSize))
-        self.upperCentreValue = Label(self.centreFrame, text=upperCentreValue, height=height, padx=padx,
+        self.upperCentreValue = Label(self.centreFrame, text=upperCentreValue, height=height, pady=pady, padx=padx,
                                       background='black',
                                       foreground='white')
         self.upperCentreValue.grid(column=0, row=1, sticky=N)
         self.upperCentreValue.config(font=(font, bigFontSize))
 
-        self.upperRightLabel = Label(self.rightFrame, text=upperRightLabel, width=width, padx=padx, background='black',
+        self.upperRightLabel = Label(self.rightFrame, text=upperRightLabel, width=width, padx=padx, pady=pady,
+                                     background='black',
                                      foreground='white')
         self.upperRightLabel.grid(column=0, row=0, sticky=E)
         self.upperRightLabel.config(font=(font, smallFontSize))
         self.upperRightValue = Label(self.rightFrame, text=upperRightValue, height=height, padx=padx,
-                                     background='black',
-                                     foreground='white')
+                                     pady=pady, background='black', foreground='white')
         self.upperRightValue.grid(column=0, row=1, sticky=E)
         self.upperRightValue.config(font=(font, bigFontSize))
 
-        self.lowerLeftLabel = Label(self.leftFrame, text=lowerLeftLabel, width=width, padx=padx, background='black',
+        self.lowerLeftLabel = Label(self.leftFrame, text=lowerLeftLabel, width=width, padx=padx, pady=pady,
+                                    background='black',
                                     foreground='white')
         self.lowerLeftLabel.grid(column=0, row=2, sticky=W)
         self.lowerLeftLabel.config(font=(font, smallFontSize))
-        self.lowerLeftValue = Label(self.leftFrame, text=lowerLeftValue, height=height, padx=padx, background='black',
+        self.lowerLeftValue = Label(self.leftFrame, text=lowerLeftValue, height=height, padx=padx, pady=pady,
+                                    background='black',
                                     foreground='white')
         self.lowerLeftValue.grid(column=0, row=3, sticky=W)
         self.lowerLeftValue.config(font=(font, bigFontSize))
 
-        self.lowerCentreLabel = Label(self.centreFrame, text=lowerCentreLabel, width=width, padx=padx,
+        self.lowerCentreLabel = Label(self.centreFrame, text=lowerCentreLabel, width=width, padx=padx, pady=pady,
                                       background='black',
                                       foreground='white')
         self.lowerCentreLabel.grid(column=0, row=2, sticky=N)
         self.lowerCentreLabel.config(font=(font, smallFontSize))
-        self.lowerCentreValue = Label(self.centreFrame, text=lowerCentreValue, height=height, padx=padx,
+        self.lowerCentreValue = Label(self.centreFrame, text=lowerCentreValue, height=height, padx=padx, pady=pady,
                                       background='black',
                                       foreground='white')
         self.lowerCentreValue.grid(column=0, row=3, sticky=N)
         self.lowerCentreValue.config(font=(font, bigFontSize))
 
-        self.lowerRightLabel = Label(self.rightFrame, text=lowerRightLabel, width=width, padx=padx, background='black',
+        self.lowerRightLabel = Label(self.rightFrame, text=lowerRightLabel, width=width, padx=padx, pady=pady,
+                                     background='black',
                                      foreground='white')
         self.lowerRightLabel.grid(column=0, row=2, sticky=E)
         self.lowerRightLabel.config(font=(font, smallFontSize))
-        self.lowerRightValue = Label(self.rightFrame, text=lowerRightValue, height=height, padx=padx,
+        self.lowerRightValue = Label(self.rightFrame, text=lowerRightValue, height=height, padx=padx, pady=pady,
                                      background='black',
                                      foreground='white')
         self.lowerRightValue.grid(column=0, row=3, sticky=E)
         self.lowerRightValue.config(font=(font, bigFontSize))
 
-    def rpmupdate(self, rpm):
+    def init_rpmbar(self, master):
+        self.rpmBarBG = self.canvas.create_rectangle(0, 0, 800, 80, fill='black')
+        self.rpmBar = self.canvas.create_rectangle(0,0,800,80, fill='black')
 
+        # self.rpmBar.grid(column=0, row=0, columnspan=3, sticky=W)
+        # self.canvas.tag_lower(self.rpmBar)
+
+    def draw_aesthetics(self, master):
+        self.circle = self.canvas.create_oval(-200, 20, 1600, 160, fill='green', outline='green')
+        self.blocker1 = self.canvas.create_rectangle(0, 80, 800, 360, fill='black', outline='black')
+        self.canvas.tag_raise(self.rpmBar)
+        self.horizontalRedLine1 = self.canvas.create_line(0,80,800,80,width=5,fill='red')
+        self.canvas.tag_raise(self.horizontalRedLine1)
+
+
+    def rpmupdate(self, rpm):
         if rpm < rpmmax:
-            self.rpmBar.configure(width=round(rpm / 111))
+            self.canvas.coords(self.rpmBar, round(rpm), 0, round(rpm / 111)+800, 80)
         else:
-            self.rpmBar.configure(width=round(rpmmax / 111))
+            self.canvas.coords(self.rpmBar, round(rpmmax), 0, round(rpmmax / 111)+800, 80)
 
     def greet(self):
         print("Greetings!")
@@ -135,6 +155,8 @@ def main():
     my_gui = MyFirstGUI(root)
     my_gui.init_all_frames(root)
     my_gui.init_all_dials(root)
+    my_gui.init_rpmbar(root)
+    my_gui.draw_aesthetics(root)
     rpm = 0
     try:
         while True:
