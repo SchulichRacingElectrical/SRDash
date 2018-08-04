b3ae02efb200c95fbe6ce04f9f1e364fb1422038
WillEhman <will@ehman.com>
2018-08-04 11:07:00 -0600

    Fixed CAN Bus errors and adding library to directory

--- CAN/send_can_blink/send_can_blink.ino
+++ CAN/send_can_blink/send_can_blink.ino
@@ -23,12 +23,12 @@ void setup()
     Serial.println("CAN BUS Shield init ok!");
 }
 
-unsigned char stmp[8] = {ledHIGH, 1, 2, 3, ledLOW, 5, 6, 7};
+unsigned char message[] = "Hello World!";
 
 void loop()
 {   Serial.println("In loop");
     // send data:  id = 0x70, standard frame, data len = 8, stmp: data buf
-    CAN.sendMsgBuf(0x70, 0, 8, stmp);
+    CAN.sendMsgBuf(0x70, 0, (int)sizeof(message), message);
     delay(1000);                       // send data once per second
 }
 
