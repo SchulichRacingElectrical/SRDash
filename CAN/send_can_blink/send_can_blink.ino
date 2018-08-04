43c75543520627ec6b7ee301082556d51f285eab
WillEhman <will@ehman.com>
2018-08-04 13:34:50 -0600

    Trying to make it so we can send buffers larger than 8 with succesive CAN IDs. Not functional

--- CAN/send_can_blink/send_can_blink.ino
+++ CAN/send_can_blink/send_can_blink.ino
@@ -5,9 +5,7 @@
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
 const int SPI_CS_PIN = 10;
-const int ledHIGH    = 1;
-const int ledLOW     = 0;
-
+unsigned int received = 0;
 MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
 void setup()
@@ -21,15 +19,34 @@ void setup()
         delay(100);
     }
     Serial.println("CAN BUS Shield init ok!");
+    
 }
 
-unsigned char message[] = "Hello World!";
-
+unsigned char mymessage[] = {11,12,13,14,15,16,17,18,19,420};
+unsigned int canindex = 0x0;
 void loop()
-{   Serial.println("In loop");
-    // send data:  id = 0x70, standard frame, data len = 8, stmp: data buf
-    CAN.sendMsgBuf(0x70, 0, (int)sizeof(message), message);
-    delay(1000);                       // send data once per second
+
+{
+//    Serial.println("In loop");
+    // send data:  id = 0x70, standard frame, data len = sizeof(data), stmp: data buf
+    int i=0;
+    int j=0;
+    unsigned char packet[8];
+    while (i<sizeof(mymessage)){
+      packet[j]=mymessage[i];
+      Serial.print(packet[i]);
+      if (packet[7] != '\0'){
+//        CAN.sendMsgBuf(0x70+canindex, 0, 8, packet);
+        Serial.println("Sent message on " + 0x70+canindex);
+        canindex+=0xF;
+        j=0;
+        memset(packet,'\0',8);
+        
+      }
+      i++;
+      j++;
+    }
+    delay(1000);
 }
 
 /*********************************************************************************************************
